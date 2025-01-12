62b5b2ee84702e9f05c0de016bfda6174a368dd4
akkaky <nikolay.korotin@outlook.com>
2025-01-12 22:04:57 +0300

    make clickable logo

--- js/app.js
+++ js/app.js
@@ -28,10 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
             let x = window.matchMedia("(max-width: 620px)")
             if (x.matches) { // If media query matches
-                menuLinks.classList.toggle("active")
+                menuLinks.classList.remove("active")
             }
         });
     });
 
+    let l = document.querySelector('.logo')
+    l.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        const targetId = this.getAttribute('href'); // Get the target ID
+        const targetElement = document.querySelector(targetId); // Find the target element
+
+        // Smoothly scroll to the target element
+        window.scrollTo({
+            top: targetElement.offsetTop - 50, // Adjust for menu height
+            behavior: 'smooth' // Smooth scrolling
+        });
+
+        let x = window.matchMedia("(max-width: 620px)")
+        if (x.matches) { // If media query matches
+            menuLinks.classList.remove("active")
+        }
+    })
 });
